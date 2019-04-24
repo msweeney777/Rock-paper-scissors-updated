dceb9bac470f017359ecf8da0986318c2a946ded
Matt Sweeney <msweeney777@gmail.com>
2019-04-24 14:32:36 -0400

    Eliminated lowdb and pertinent files; created a stats.json file for storage of score between inputs

--- index.mjs
+++ index.mjs
@@ -1,8 +1,9 @@
-import {Stats} from './db.mjs';
+import fs from 'fs';
 const choices = ['rock', 'paper', 'scissors'];
-const stats = {'win': 0, 'lose': 0, 'tie': 0}
+const stats = {'win': 0, 'lose': 0, 'tie': 0};
 let response = "";
 let result = "";
+
 
 const results = {
   rock: {
@@ -22,6 +23,16 @@ const results = {
   }
 }
 
+function setScore() {
+  let stat = {win: 0, lose: 0, tie: 0};
+  let data = JSON.stringify(stat, null, 2);
+  fs.writeFile('stats.json', data, (err) => {
+    if (err) throw err;
+  console.log("Default score created in stats.json");
+});
+
+}
+
 function respond(choice) {
   return result = results[choice][response];
 }
@@ -31,23 +42,48 @@ function machine () {
 }
 
 function statistics () {
-  Stats.find({id: result}).update('total', n => n + 1).write();
-  console.log(`
-  human: ${Stats.find({id: 'win'}).get('total').value()}
-  machine: ${Stats.find({id: 'lose'}).get('total').value()}
-  tie: ${Stats.find({id: 'tie'}).get('total').value()}\n`);
+  fs.readFile('stats.json', (err, data) => {
+    if (err) throw err;
+    let stat = JSON.parse(data);
+    stat[result]++;
+  
+    stat = JSON.stringify(stat, null, 2);
+
+    fs.writeFile('stats.json', stat, (err) => {
+    if (err) throw err;
+
+    console.log(`
+    human: ${JSON.parse(stat).win} 
+    machine: ${JSON.parse(stat).lose}
+    tie: ${JSON.parse(stat).tie}\n`);
+
+    });
+  });
 }
 
 function deleteScore() {
-  Stats.find({id: 'win'}).update('total', n => n = 0).write();
-  Stats.find({id: 'lose'}).update('total', n => n = 0).write();
-  Stats.find({id: 'tie'}).update('total', n => n = 0).write();
+
+  fs.readFile('stats.json', (err, data) => {
+    if (err) throw err;
+    let stat = JSON.parse(data);
+
+    stat["win"] = 0;
+    stat["lose"] = 0;
+    stat["tie"] = 0;
+
+    stat = JSON.stringify(stat, null, 2);
+
+    fs.writeFile('stats.json', stat, (err) => { 
+    if (err) throw err;
+    console.log('\nScore reset.\n');
+
+    });
+  });
 }
 
 const flag = (process.argv.slice(2, 3)).pop();
 
 const choice = (process.argv.slice(3)).pop();
-//console.log(`\n${choice}`);
 
 if(choices.includes(choice) && (flag == '-i')) {
   machine();
@@ -60,13 +96,11 @@ if(choices.includes(choice) && (flag == '-i')) {
   } else {
     console.log('Tie.')
   }
-  statistics(result)
+  statistics();
 } else if(flag == '-d') {
   deleteScore();
-  console.log('\nScore deleted.\n');
-  statistics();
 } else if(flag == '-m') {
-  console.log('\nInput: [-i] rock/paper/scissors\n\nDelete: [-d]\n')
+  console.log('\nInput: [-i] rock/paper/scissors\n\nReset score: [-d]\n')
 } else {
   console.log('\nError ... cannot process given input. Try again.\n');
   console.log('See menu "-m" for details on proper entry syntax\n')
